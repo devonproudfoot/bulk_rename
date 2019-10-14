d9e093eb424e68fbae05afae1f1ba481bf3bc738
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-10-14 12:23:08 -0500

    Changed file to utilize the identifiers from a separate file

--- bulk_rename_files.py
+++ bulk_rename_files.py
@@ -1,12 +1,11 @@
 import os, shutil, csv, sys, argparse
 from datetime import datetime
+import identifiers
 
 class file_info:
     hidden_files = ['Thumbs.db', 'ehthumbs.db', '.DS_Store', '.AppleDouble', '.LSOverride']
     file_metadata = ['old_filename', 'new_filename', 'filepath']
     filetypes = ['.png', '.tif', '.jpg', '.doc', '.docx', '.pdf', '.csv']
-    filepath = ''
-    identifier = 1
 
     def create_logging_csv(self, renamed_files):
         date = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -27,6 +26,7 @@ class file_info:
 files = file_info()
 files_changed = []
 directory = 'to_change'
+current_id = identifiers.next_id
 
 for folder in os.listdir(directory):
     if folder not in files.hidden_files:
@@ -35,7 +35,7 @@ for folder in os.listdir(directory):
             if old_filename not in files.hidden_files:
                 old_path = os.path.join(folder_path, old_filename)
                 file_extension = files.check_filetype(old_filename)
-                new_filename = str(files.identifier) + file_extension
+                new_filename = str(current_id) + file_extension
                 new_path = os.path.join(folder_path, new_filename)
                 shutil.move(old_path, new_path)
                 dictionary_changes = {
@@ -44,6 +44,6 @@ for folder in os.listdir(directory):
                     'filepath' : folder
                 }
                 files_changed.append(dictionary_changes)
-                files.identifier += 1
+                current_id += 1
 
 files.create_logging_csv(files_changed)
