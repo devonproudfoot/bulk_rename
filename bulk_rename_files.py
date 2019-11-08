6cb5522faf2387cf107a0a381cd4ec5ef14ba807
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-11-08 10:34:45 -0600

    spaces to tabs

--- bulk_rename_files.py
+++ bulk_rename_files.py
@@ -3,37 +3,37 @@ from datetime import datetime
 import identifiers
 
 class file_info:
-    hidden_files = ['Thumbs.db', 'ehthumbs.db', '.DS_Store', '.AppleDouble', '.LSOverride']
-    file_metadata = ['old_filename', 'new_filename', 'filepath']
-    filetypes = ['.png', '.tif', '.jpg', '.doc', '.docx', '.pdf', '.csv']
+	hidden_files = ['Thumbs.db', 'ehthumbs.db', '.DS_Store', '.AppleDouble', '.LSOverride']
+	file_metadata = ['old_filename', 'new_filename', 'filepath']
+	filetypes = ['.png', '.tif', '.jpg', '.doc', '.docx', '.pdf', '.csv']
 
-    def create_logging_csv(self, renamed_files):
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        csv_name = 'updated_filenames_' + date + '.csv'
-        with open(csv_name, 'w') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=self.file_metadata)
-            writer.writeheader()
-            writer.writerows(renamed_files)
+	def create_logging_csv(self, renamed_files):
+		date = datetime.now().strftime("%Y%m%d%H%M%S")
+		csv_name = 'updated_filenames_' + date + '.csv'
+		with open(csv_name, 'w') as csv_file:
+			writer = csv.DictWriter(csv_file, fieldnames=self.file_metadata)
+			writer.writeheader()
+			writer.writerows(renamed_files)
 
-    def check_filetype(self, file):
-        for filetype in self.filetypes:
-            if file.endswith(filetype):
-                return filetype
+	def check_filetype(self, file):
+		for filetype in self.filetypes:
+			if file.endswith(filetype):
+				return filetype
 
-    def logging_csv_to_dictionary(self, spreadsheet_file):
-        new_dictionary = []
-        with open(spreadsheet_file) as spreadsheet:
-            csvreader = csv.DictReader(spreadsheet)
+	def logging_csv_to_dictionary(self, spreadsheet_file):
+		new_dictionary = []
+		with open(spreadsheet_file) as spreadsheet:
+			csvreader = csv.DictReader(spreadsheet)
 
-    def prompt_for_choice(self):
-        selection = input('Enter 1 if you are renaming new files, or 2 if you are renaming from a spreadsheet: ')
-        if selection == '1':
-            return 1
-        elif selection == '2':
-            return 2
-        else:
-            print('Invalid response! Run script again and choose 1 or 2!')
-            sys.exit()
+	def prompt_for_choice(self):
+		selection = input('Enter 1 if you are renaming new files, or 2 if you are renaming from a spreadsheet: ')
+		if selection == '1':
+			return 1
+		elif selection == '2':
+			return 2
+		else:
+			print('Invalid response! Run script again and choose 1 or 2!')
+			sys.exit()
 
 files = file_info()
 files_changed = []
@@ -42,46 +42,46 @@ current_id = identifiers.next_id
 choice = files.prompt_for_choice()
 
 if choice == 1:
-    for folder in os.listdir(directory):
-        if folder not in files.hidden_files:
-            folder_path = os.path.join(directory, folder)
-            for old_filename in os.listdir(folder_path):
-                if old_filename not in files.hidden_files:
-                    old_path = os.path.join(folder_path, old_filename)
-                    file_extension = files.check_filetype(old_filename)
-                    new_filename = str(current_id) + file_extension
-                    new_path = os.path.join(folder_path, new_filename)
-                    shutil.move(old_path, new_path)
-                    dictionary_changes = {
-                        'old_filename' : old_filename,
-                        'new_filename' : new_filename,
-                        'filepath' : folder
-                    }
-                    files_changed.append(dictionary_changes)
-                    current_id += 1
+	for folder in os.listdir(directory):
+		if folder not in files.hidden_files:
+			folder_path = os.path.join(directory, folder)
+			for old_filename in os.listdir(folder_path):
+				if old_filename not in files.hidden_files:
+					old_path = os.path.join(folder_path, old_filename)
+					file_extension = files.check_filetype(old_filename)
+					new_filename = str(current_id) + file_extension
+					new_path = os.path.join(folder_path, new_filename)
+					shutil.move(old_path, new_path)
+					dictionary_changes = {
+						'old_filename' : old_filename,
+						'new_filename' : new_filename,
+						'filepath' : folder
+					}
+					files_changed.append(dictionary_changes)
+					current_id += 1
 
-    files.create_logging_csv(files_changed)
+	files.create_logging_csv(files_changed)
 elif choice == 2:
-    files_to_change_path = 'change_from_csv'
-    csv_for_renaming = 'for_renaming.csv'
-    for folder in os.listdir(files_to_change_path):
-        if folder not in files.hidden_files:
-            folderpath = os.path.join(files_to_change_path, folder)
-            with open(csv_for_renaming) as csv_filenames:
-                csv_reader = csv.DictReader(csv_filenames)
-                for row in csv_reader:
-                    new_filename = row['new_filename'].replace('.tif', '.jpg')
-                    old_filename = row['old_filename'].replace('.tif', '.jpg')
-                    full_old_filepath = os.path.join(folderpath, old_filename)
-                    full_new_filepath = os.path.join(folderpath, new_filename)
-                    if os.path.exists(full_old_filepath):
-                        shutil.move(full_old_filepath, full_new_filepath)
-                        dictionary_changes = {
-                            'old_filename' : old_filename,
-                            'new_filename' : new_filename,
-                            'filepath' : row['filepath']
-                        }
-                        files_changed.append(dictionary_changes)
-        print("Completed {}".format(folder))
-            
-    files.create_logging_csv(files_changed)
+	files_to_change_path = 'change_from_csv'
+	csv_for_renaming = 'for_renaming.csv'
+	for folder in os.listdir(files_to_change_path):
+		if folder not in files.hidden_files:
+			folderpath = os.path.join(files_to_change_path, folder)
+			with open(csv_for_renaming) as csv_filenames:
+				csv_reader = csv.DictReader(csv_filenames)
+				for row in csv_reader:
+					new_filename = row['new_filename'].replace('.tif', '.jpg')
+					old_filename = row['old_filename'].replace('.tif', '.jpg')
+					full_old_filepath = os.path.join(folderpath, old_filename)
+					full_new_filepath = os.path.join(folderpath, new_filename)
+					if os.path.exists(full_old_filepath):
+						shutil.move(full_old_filepath, full_new_filepath)
+						dictionary_changes = {
+							'old_filename' : old_filename,
+							'new_filename' : new_filename,
+							'filepath' : row['filepath']
+						}
+						files_changed.append(dictionary_changes)
+		print("Completed {}".format(folder))
+			
+	files.create_logging_csv(files_changed)
